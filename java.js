26b9938dc6a17d6f42c207c3a05643d6d0496cd9
MRkiller192k <111495103+MRkiller192k@users.noreply.github.com>
2022-11-30 23:09:18 +0100

    Add files via upload

---
+++ java.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("manyjavasricpt").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("manyjavasricpt").style.width = "0%";
+  }
